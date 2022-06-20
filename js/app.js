3f8ccea70ee28dab77366eb972030d3f60ba23a6
jisha66 <mydreamplace2020@gmail.com>
2022-06-20 15:50:17 +0600

    Forth Added

--- js/app.js
+++ js/app.js
@@ -13,6 +13,45 @@ $(document).ready(() => {
         loop: true,
         nav: true,
         navText: navText,
+        autoplay: true,
         autoplayHoverPause: true
+    })
+
+    $('#top-movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        loop: true,
+        autoplay: false,
+        autoplayHoverPause: true,
+        responsive: {
+            500: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+
+    $('.movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        nav: true,
+        navText: navText,
+        margin: 15,
+        responsive: {
+            500: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
     })
 })
